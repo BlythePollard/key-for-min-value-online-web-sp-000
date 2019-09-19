45be66245f9109a2e3d39a4756716b50e0541bc8
Blythe Pollard <blytheepollard@gmail.com>
2019-09-19 22:52:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,9 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   name_hash.each do |key, value| 
     if lowest_value == nil
-      
-    if value < lowest_value 
+      lowest_value = value
+      lowest_key = key
+    elsif value < lowest_value 
       lowest_value = value 
       lowest_key = key
 end
