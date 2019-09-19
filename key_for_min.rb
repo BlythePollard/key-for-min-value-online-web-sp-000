45c244645c0293293a1127925aa65799cfd648a9
Blythe Pollard <blytheepollard@gmail.com>
2019-09-19 22:40:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,6 +11,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value| 
     if value < lowest_value 
       value = lowest_value
+      return value
       lowest_key = key
 end
 return lowest_key
