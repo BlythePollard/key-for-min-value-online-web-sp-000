afadeb0910b5f6240d5acab913434e9ac6e3685f
Blythe Pollard <blytheepollard@gmail.com>
2019-09-19 22:48:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   name_hash.each do |key, value| 
     if value == nil
-      
+       value = FLOAT::INFINITY
     if value < lowest_value 
       lowest_value = value 
       lowest_key = key
