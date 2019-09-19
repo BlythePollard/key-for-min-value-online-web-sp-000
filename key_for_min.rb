f895a54dbbdf627d263cf03351d832db22dcef7b
Blythe Pollard <blytheepollard@gmail.com>
2019-09-19 22:45:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@
 
 
 def key_for_min_value(name_hash)
-  lowest_value = Float::INFINITY #do I need to say = nil?
+  lowest_value = nil #do I need to say = nil?
   lowest_key = nil
   name_hash.each do |key, value| 
     if value == nil || value < lowest_value 
