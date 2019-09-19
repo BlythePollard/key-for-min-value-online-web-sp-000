75c948fda29de81cb3e6daa3d937149e0053a453
Blythe Pollard <blytheepollard@gmail.com>
2019-09-19 22:34:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil #do I need to say = nil?
   lowest_key = nil
   name_hash.each do |key, value| 
+      value = lowest_value
     if value < lowest_value 
       value = lowest_value
       lowest_key = key
